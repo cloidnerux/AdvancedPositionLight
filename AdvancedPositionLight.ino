eb5efd2d53bb5667bccf59448d577a8591a76d49
cloidnerux <cloidnerux@gmail.com>
2015-07-18 20:45:48 +0200

    Refactoring

--- AdvancedPositionLight.ino
+++ AdvancedPositionLight.ino
@@ -1,14 +1,10 @@
-//#include <GCS_MAVLink.h>
+/// Advanced Position Light
+/// Based upon an arduino pro mini
+/// by cloidnerux, 2015
 
 
-
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
-//#include <FastSerial.h>
 #include <../GCS_MAVLink/include/mavlink/v1.0/mavlink_types.h>        // Mavlink interface
-//#include <../GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/mavlink.h>
 #include <../GCS_MAVLink/include/mavlink/v1.0/common/mavlink.h>
-//#include <../GCS_MAVLink/include/mavlink/v1.0/common/mavlink_msg_request_data_stream.h>
 
 
 #include <Adafruit_NeoPixel.h>
@@ -17,60 +13,62 @@
 #endif
 
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define PIN            4
-
-// How many NeoPixels are attached to the Arduino?
+// How many WS2812 are attached to the Arduino in one string
 #define NUMPIXELS      8
 
+//Which voltage is to considerd good
 #define BATTERY_GOOD 4000
+//Which voltage is consider accapable
 #define BATTERY_OK   3600
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
+//The string of WS2812
 Adafruit_NeoPixel strip_1 = Adafruit_NeoPixel(NUMPIXELS, 4, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip_2 = Adafruit_NeoPixel(NUMPIXELS, 5, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip_3 = Adafruit_NeoPixel(NUMPIXELS, 6, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip_4 = Adafruit_NeoPixel(NUMPIXELS, 7, NEO_GRB + NEO_KHZ800);
 
-int delayval = 500; // delay for half a second
+
+////////////////////////////////////////////////////////////////////////////////////
 
 uint8_t    ap_fixtype = 3;                  //   0= No GPS, 1 = No Fix, 2 = 2D Fix, 3 = 3D Fix
 uint8_t    ap_sat_visible = 0;
-// Message # 1  SYS_STATUS 
+
 uint16_t   ap_voltage_battery = 0;    // 1000 = 1V
 int16_t    ap_current_battery = 0;    //  10 = 1A
 uint8_t    ap_cell_count = 0;
 
+////////////////////////////////////////////////////////////////////////////////////
+
 uint8_t updateSys;
 uint8_t updateGps;
 
+////////////////////////////////////////////////////////////////////////////////////
 static uint8_t      apm_mav_type;
 static uint8_t      apm_mav_system; 
 static uint8_t      apm_mav_component;
 static uint8_t      base_mode=0;
-static unsigned long        lastMAVBeat = 0;
 
 mavlink_message_t msg; 
 uint8_t buf[50];
 int system_type = MAV_TYPE_QUADROTOR;
 int autopilot_type = MAV_AUTOPILOT_GENERIC;
 
+////////////////////////////////////////////////////////////////////////////////////
+
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  //startup all diferent modules
   strip_1.begin();
   strip_2.begin();
   strip_3.begin();
   strip_4.begin();
-  Serial.begin(57600);
+  Serial.begin(115200);
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   updateSys = 0;
   updateGps = 0;
-  for(int i=0;i<NUMPIXELS;i++){
 
+  //Initialise the LEDs
+  for(int i=0;i<NUMPIXELS;i++){
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     strip_1.setPixelColor(i, strip_1.Color(0,0,0)); // Moderately bright green color.
     strip_2.setPixelColor(i, strip_2.Color(0,0,0)); // Moderately bright green color.
@@ -82,15 +80,8 @@ void setup() {
   strip_3.show();
   strip_4.show();
 
-  uint8_t len;
-  
+  //Wait a moment for the APM/Pixhawk to boot
   delay(3000);
-  for(int i = 0; i < 3; i++)
-  {
-    len = mavlink_msg_request_data_stream_pack(MAVLINK_COMM_0, 200, &msg, system_type, autopilot_type, MAV_DATA_STREAM_EXTENDED_STATUS, 2, 1);
-    Serial.write(buf, len);
-    delay(500);
-  }
 }
 
 void loop() {
@@ -100,28 +91,20 @@ void loop() {
   static uint8_t state = 0;
   static uint8_t connection = 0;
   uint8_t voltageAlarm;
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-  
-  
-  // Pack the message
-  // mavlink_message_heartbeat_pack(system id, component id, message container, system type, MAV_AUTOPILOT_GENERIC)
-  //mavlink_msg_heartbeat_pack(100, 200, &msg, system_type, autopilot_type, 0, 0, 0);
-	
-  // Copy the message to send buffer 
-  //uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
-	
-  // Send the message (.write sends as bytes) 
-  //Serial.write(buf, len);
-  
+
+  //Receive Mavlink packages
   comm_receive();
+  //Do we have valid voltage data?
   if(ap_cell_count > 1)
   {
+    //Is the battery empty, so we have to give a low voltage alarm indicator
     if((ap_voltage_battery / ap_cell_count) < BATTERY_OK)
       voltageAlarm = 1;
     else
       voltageAlarm = 0;
   }
-  
+
+  //Switch every second
   if((millis() - time) > 1000)  //tick
   {
     time = millis();
@@ -149,6 +132,7 @@ void loop() {
     strip_3.show();
     strip_4.show();
   }
+  //We have new cell voltage data
   if(ap_cell_count > 1 && updateSys)
   {
     updateSys = 0;
@@ -168,6 +152,7 @@ void loop() {
        strip_2.show();
     }
   }
+  //There is new GPS data to process and show
   if(updateGps)
   {
     updateGps = 0;
@@ -218,15 +203,7 @@ void comm_receive() {
           {
               apm_mav_system    = msg.sysid;
               apm_mav_component = msg.compid;
-              apm_mav_type      = mavlink_msg_heartbeat_get_type(&msg);            
-           //   osd_mode = mavlink_msg_heartbeat_get_custom_mode(&msg);
-             // osd_mode = (uint8_t)mavlink_msg_heartbeat_get_custom_mode(&msg);
-              //Mode (arducoper armed/disarmed)
-             // base_mode = mavlink_msg_heartbeat_get_base_mode(&msg);
-             // if(getBit(base_mode,7)) motor_armed = 1;
-             // else motor_armed = 0;
-  
-              lastMAVBeat = millis();
+              apm_mav_type      = mavlink_msg_heartbeat_get_type(&msg);              
           }
           break;
         case MAVLINK_MSG_ID_SYS_STATUS:
@@ -253,7 +230,6 @@ void comm_receive() {
         break;
       }
     } 
-    // And get the next one
   }
 }
 
